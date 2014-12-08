4b82d508ed10b267147278e643e3c13b8def71b7
troy davis <troy.davis@kearsarge.org>
2014-12-08 09:00:00 -0500

    ignore and something in game...probably nothing but whatevs

--- game.py
+++ game.py
@@ -1,1 +1,44 @@
+import pygame, sys, random
+#from Ball import Ball
+#from PlayerBall import PlayerBall
 
+pygame.init()
+
+clock = pygame.time.Clock()
+
+width = 800 
+height = 600
+size = width, height
+
+bgColor = r,g,b = 0, 0, 0
+#bgI = pygame.image.load("dayzeyroimages/dayz zeyro/Resources/Object/Background/New Road.jpg")
+#bgR = bgI.get_rect()
+
+screen = pygame.display.set_mode(size)
+
+player = PlayerBall([width/2, height/2])
+
+balls = []
+balls += [Ball("images/Ball/ball.png", [4,5], [100, 125])]
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_w or event.key == pygame.K_UP:
+				player.go("up")
+			if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+				player.go("right")
+			if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+				player.go("down")
+			if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+				player.go("left")
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_w or event.key == pygame.K_UP:
+				player.go("stop up")
+			if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+				player.go("stop right")
+			if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+				player.go("stop down")
+			if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+				player.go("stop left")
