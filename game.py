a39daff8a421aa47bf88ac7e30ad3733c7c2d20a
Samuel Bourcier <samuel.bourcier@kearsarge.org>
2014-12-15 09:30:44 -0500

    Put player image in code

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame, sys, random
-#from Ball import Ball
-#from PlayerBall import PlayerBall
+from Ball import Ball
+from Player import Player
+#from HUD import Text
+#from HUD import Score
 
 pygame.init()
 
@@ -9,17 +11,19 @@ clock = pygame.time.Clock()
 width = 800 
 height = 600
 size = width, height
+bg = pygame.image.load("Resources/Object/Background/NewRoad.png")
 
-bgColor = r,g,b = 0, 0, 0
-#bgI = pygame.image.load("dayzeyroimages/dayz zeyro/Resources/Object/Background/New Road.jpg")
-#bgR = bgI.get_rect()
-
+#bgColor = r,g,b = 200, 0, 200
 screen = pygame.display.set_mode(size)
-
-player = PlayerBall([width/2, height/2])
+bgrect = bg.get_rect()
+player = Player([width/2, height/2])
 
 balls = []
-balls += [Ball("images/Ball/ball.png", [4,5], [100, 125])]
+balls += [Ball("ball.png", [4,5], [100, 125])]
+
+#timer = Score([80, height - 25], "Time: ", 36)
+#timerWait = 0
+#timerWaitMax = 6
 
 while True:
 	for event in pygame.event.get():
@@ -42,3 +46,39 @@ while True:
 				player.go("stop down")
 			if event.key == pygame.K_a or event.key == pygame.K_LEFT:
 				player.go("stop left")
+		
+	if len(balls) < 10:
+		if random.randint(0, .25*60) == 0:
+			balls += [Ball("ball.png",
+					  [random.randint(0,10), random.randint(0,10)],
+					  [random.randint(100, width-100), random.randint(100, height-100)])
+					  ]
+					  
+	#if timerWait < timerWaitMax:
+	#	timerWait += 1
+	#else:
+	#	timerWait = 0
+	#	timer.increaseScore(.1)
+	player.update(width, height)
+	#timer.update()
+	for ball in balls:
+		ball.update(width, height)
+		
+	#for bully in balls:
+		#for victem in balls:
+	#		bully.collideBall(victem)
+	#		bully.collidePlayer(player)
+	
+	#for ball in balls:
+	#	if not ball.living:
+	#		balls.remove(ball)
+	
+	#bgColor = r,g,b
+	#screen.fill(bgColor)
+	screen.blit(bg,bgRect)
+	for ball in balls:
+		screen.blit(ball.image, ball.rect)
+	screen.blit(player.image, player.rect)
+	#screen.blit(timer.image, timer.rect)
+	pygame.display.flip()
+	#clock.tick(60)
